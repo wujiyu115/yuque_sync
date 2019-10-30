fe7d8ad8af7ea503e1d8f102b9eb60ef038c01ac
wujiyu <wujiyu115@gmail.com>
2019-10-30 19:41:18 +0800

    deal front matter

--- adapter/hexo.go
+++ adapter/hexo.go
@@ -1,1 +1,19 @@
 package adapter
+
+import (
+	"html"
+	"regexp"
+
+	"github.com/wujiyu115/yuqueg"
+)
+
+func parseMatter(body string) {
+
+}
+
+//GenHexo of gen
+func GenHexo(post yuqueg.Doc) string {
+	body := html.UnescapeString(post.Body)
+	brTag, _ := regexp.Compile("(title:|layout:|tags:|date:|categories:){1}(\\S|\\s)+?---")
+	return brTag.ReplaceAllString(body, "\n")
+}
